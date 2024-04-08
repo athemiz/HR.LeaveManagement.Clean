72846e3b1640450b44b93aae6f3e1d8a37619c3b
Otavio Caldonazo <otavio.caldonazo@fortrobotics.com>
2024-04-08 11:53:20 -0300

    Update application

---
+++ HR.LeaveManagement.Application/Contracts/Identity/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Application.Models.Identity;
+
+namespace HR.LeaveManagement.Application.Contracts.Identity
+{
+    public interface IUserService
+    {
+        Task<List<Employee>> GetEmployees();
+        Task<Employee> GetEmployee();
+    }
+}
---
+++ HR.LeaveManagement.Identity/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Application.Models.Identity;
+
+namespace HR.LeaveManagement.Application.Contracts.Identity
+{
+    public class UserService : IUserService
+    {
+        public Task<Employee> GetEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Employee>> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
